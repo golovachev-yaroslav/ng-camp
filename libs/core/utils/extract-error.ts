0fab506e250cbc9ea9f23c891915b6c9cbc8a5e5
GolovachevYaroslav <golovachev@saritasa.com>
2025-03-14 18:59:05 +0500

    JC-669 resolved linter issues

--- libs/core/utils/extract-error.ts
+++ libs/core/utils/extract-error.ts
@@ -1,25 +1,12 @@
 import { EntityValidationErrors } from '../models/app-error';
 
 /**
- * Extract an error from the API response.
- * @param error List of errors.
- * @returns The first error.
- */
-export function extractError(error: unknown): string | undefined {
-	if (Array.isArray(error)) {
-		return error[0];
-	}
-
-	return undefined;
-}
-
-/**
  * Finds and extract errors by attribute.
  * @param errorsDto Validation DTO errors.
  * @param attribute Attribute.
  */
 export function extractErrorMessages(
-	errorsDto: EntityValidationErrors<any>[] | null | undefined,
+	errorsDto: EntityValidationErrors<Record<string, string>>[] | null | undefined,
 	attribute: string | null,
 ): string | undefined {
 	return errorsDto
